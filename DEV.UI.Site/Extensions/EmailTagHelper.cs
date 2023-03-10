5adb122f232cc0c1c1ffd6962bfba05c9bf20db9
Ronaldeived Silva <rona_dev@outlook.com>
2023-03-10 15:21:24 -0300

    first commit

---
+++ DEV.UI.Site/Extensions/EmailTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DEV.UI.Site.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string EmailDomain { get; set; } = "dev.io";
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + EmailDomain;
+
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
+
---
+++ DEV.UI.Site/Program.cs
@@ -0,0 +1,19 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
