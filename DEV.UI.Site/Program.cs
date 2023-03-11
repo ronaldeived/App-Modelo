dcab0381838c79b274d89eb319283fc5017b51f3
Ronaldeived Silva <rona_dev@outlook.com>
2023-03-10 21:07:58 -0300

    Adicionando areas

--- DEV.UI.Site/Program.cs
+++ DEV.UI.Site/Program.cs
@@ -9,11 +9,20 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler();
 }
 
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.MapControllerRoute("areas", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+
+// Rota de áreas especializadas
+//app.MapAreaControllerRoute("AreaProdutos", "Produtos", "Produtos/{controller=Cadastro}/{action=Index}/{id?}");
+//app.MapAreaControllerRoute("AreaVendas", "Vendas", "Vendas/{controller=Pedidos}/{action=Index}/{id?}");
 
 app.Run();
 
